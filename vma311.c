97a98740845e8c2c103684cfda3e729ea0597da7
Marie-Bardet <81524101+Marie-Bardet@users.noreply.github.com>
2021-03-30 22:28:28 +0200

    Update vma311.c

--- vma311.c
+++ vma311.c
@@ -1,8 +1,8 @@
 #include "vma311.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "freertos/FreeRTOS.h" //to run freertos on esp32
+#include "freertos/task.h"//provide the multitaksing functionality
 
-static vma311_t vma311; //creates a an instance of a vma311 structure
+static vma311_t vma311; //creates a a static instance of a vma311 structure
 
 static void vma311_send_start_signal();
 static int vma311_wait(uint16_t, int);
@@ -10,25 +10,25 @@ static int vma311_check_response();
 static inline vma311_status_t vma311_read_byte(uint8_t *);
 static vma311_status_t vma311_check_crc(uint8_t *);
 
-void vma311_init(gpio_num_t num) //initializes the gpio
+void vma311_init(gpio_num_t num) // gpio initialization
 {
     vma311.num = num; //gives the pin value to the vma311 object's pin attribute
     vma311.last_read_time = -2000000;
     gpio_reset_pin(num);
-    vTaskDelay(pdMS_TO_TICKS(1000)); //waits 1 sec before sending any instruction to the sensor
+    vTaskDelay(pdMS_TO_TICKS(1000)); //waits 1 sec before sending instruction
 }
 
 vma311_data_t vma311_get_values()
 {
-    vma311_data_t error_data = {VMA311_TIMEOUT_ERROR, -1, -1, -1, -1}; //in case there's a timeout error, the value of the bytes is -1
-    uint8_t data[5] = {0, 0, 0, 0, 0};                                //initialize an array to store the values of each of the 5 bytes sent by the sensor
-    if (esp_timer_get_time() - 2000000 < vma311.last_read_time)//if the duration since the last reading of the sensor is under 2000000 ticks, the program returns the previous measures that the sensor has done
+    vma311_data_t error_data = {VMA311_TIMEOUT_ERROR, -1, -1, -1, -1}; //in case the time for sending data is too long, the value of the bytes is -1
+    uint8_t data[5] = {0, 0, 0, 0, 0};                                //initialize an array to store 5 bytes sent by the sensor
+    if (esp_timer_get_time() - 2000000 < vma311.last_read_time)//if the duration since the last reading is under 2000000 ticks, the program returns the measurement
     {
         return vma311.data;
     }
-    vma311.last_read_time = esp_timer_get_time();//otherwise we keep going and we set the time of the last reading to the beginning of this reading
+    vma311.last_read_time = esp_timer_get_time();//otherwise we continue and we set the time of the last reading to the beginning of this reading
     vma311_send_start_signal();//this function "wakes" the sensor up so it initializes and sends data
-    if (vma311_check_response() == VMA311_TIMEOUT_ERROR)// if there's any abnormal latency in communication from the sensor, there is a communication problem
+    if (vma311_check_response() == VMA311_TIMEOUT_ERROR)// if the communication is too long
     {
         return error_data;
     }
@@ -39,7 +39,7 @@ vma311_data_t vma311_get_values()
             return error_data;
         }
     }
-    if (vma311_check_crc(data) != VMA311_CRC_ERROR)// if there's no transmission error, then the data sent by the sensor is correct and we assign them to the sensor object
+    if (vma311_check_crc(data) != VMA311_CRC_ERROR)// if there's no transmission error, we assign them to the sensor object
     {
         vma311.data.rh_int = data[0];
         vma311.data.rh_dec = data[1];
@@ -47,22 +47,22 @@ vma311_data_t vma311_get_values()
         vma311.data.t_dec = data[3];
         vma311.data.status = VMA311_OK;
     }
-    return vma311.data;// we return the temperature and humidity values
+    return vma311.data;// temperature and humidity
 }
 
-void vma311_send_start_signal()// this code indicates the different steps for sending a start signal to the sensor, so it returns data
+void vma311_send_start_signal()// sending a start signal to the sensor
 {
     gpio_set_direction(vma311.num, GPIO_MODE_OUTPUT);
     gpio_set_level(vma311.num, 0);
-    ets_delay_us(20 * 1000);//set the gpio to 0 for at least 18 ms (datasheet page 6)
+    ets_delay_us(20 * 1000);//set the gpio to 0 for at least 18 ms 
     gpio_set_level(vma311.num, 1);
-    ets_delay_us(40);//set the gpio to 1 for max 40 us (datasheet page 6)
+    ets_delay_us(40);//set the gpio to 1 for max 40 us
 }
 
 int vma311_wait(uint16_t us, int level)
 {
     int us_ticks = 0;
-    while (gpio_get_level(vma311.num) == level) // while the level (1 or 0) of the pin is equal to the level in parameter
+    while (gpio_get_level(vma311.num) == level) //level of the pin is equal to the level in parameter
     {
         if (us_ticks++ > us) // we increment us_tick at every us, if it's bigger than the delay in parameter, there's a timeout
         {
@@ -77,19 +77,19 @@ int vma311_check_response()
 {
     gpio_set_direction(vma311.num, GPIO_MODE_INPUT);
     //the response from the sensor normally begins with a low voltage level on the pin for 80 us
-    if (vma311_wait(81, 0) == VMA311_TIMEOUT_ERROR) // if it stays on 0 for longer, there is a problem
+    if (vma311_wait(81, 0) == VMA311_TIMEOUT_ERROR) // if it stays on 0 for loner
     {
         return VMA311_TIMEOUT_ERROR;
     }
     //then the sensor sets the level on the pin to 1 for 80 us
-    if (vma311_wait(81, 1) == VMA311_TIMEOUT_ERROR) //if it stays on 1 for longer, there is a problem
+    if (vma311_wait(81, 1) == VMA311_TIMEOUT_ERROR) //if it stays on 1 for longer
     {
         return VMA311_TIMEOUT_ERROR;
     }
     return VMA311_OK;
 }
 
-vma311_status_t vma311_read_byte(uint8_t *byte) // this function reads one byte (there are 5 bytes in total)
+vma311_status_t vma311_read_byte(uint8_t *byte) // this function reads one byte 
 {
     for (int i = 0; i < 8; ++i)
     {
@@ -97,11 +97,10 @@ vma311_status_t vma311_read_byte(uint8_t *byte) // this function reads one byte 
         {
             return VMA311_TIMEOUT_ERROR;
         }
-        //if the high level on the pin is longer than 28us it means that the sensor is sending a bit of one
         if (vma311_wait(70, 1) > 28)
         {
-            *byte |= (1 << (7 - i)); //the sensor sends the highest bit first, so the first bit needs 7 shifts to the left
-        }                            //, the second 6 shifts and so on...
+            *byte |= (1 << (7 - i)); //the sensor sends the highest bit first
+        }                            
     }
     return VMA311_OK;
 }
@@ -113,7 +112,7 @@ vma311_status_t vma311_check_crc(uint8_t data[])
     {
         sum += data[i];
     }
-    if (sum != data[4])//if the sum is not equal to the checksum (stored in the last spot of the array), it means there's a communication error
+    if (sum != data[4])//if the sum is not equal to the checksum there's a communication error
     {
         return VMA311_CRC_ERROR;
     }
